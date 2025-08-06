132a9b79f35a8e97e90c678804971441dfdde388
codesbyaditya <adityapulipati66@gmail.com>
2025-08-06 09:04:09 +0530

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -14,54 +14,18 @@ def model_prediction(test_image):
     return np.argmax(predictions)  # return index of max element
 
 
-# Sidebar
-st.sidebar.title("Dashboard")
-app_mode = st.sidebar.selectbox("Select Page", ["Home", "About", "Disease Recognition"])
 
-# Main Page
-if (app_mode == "Home"):
-    st.header("PLANT DISEASE RECOGNITION SYSTEM")
-    image_path = "home_page.jpeg"
-    image = Image.open(image_path)
-    st.image(image, use_column_width=True)
-    st.markdown("""
-    Welcome to the Plant Disease Recognition System! 🌿🔍
-
-    Our mission is to help in identifying plant diseases efficiently. Upload an image of a plant, and our system will analyze it to detect any signs of diseases. Together, let's protect our crops and ensure a healthier harvest!
-
-    ### How It Works
-    1. **Upload Image:** Go to the **Disease Recognition** page and upload an image of a plant with suspected diseases.
-    2. **Analysis:** Our system will process the image using advanced algorithms to identify potential diseases.
-    3. **Results:** View the results and recommendations for further action.
-
-    ### Why Choose Us?
-    - **Accuracy:** Our system utilizes state-of-the-art machine learning techniques for accurate disease detection.
-    - **User-Friendly:** Simple and intuitive interface for seamless user experience.
-    - **Fast and Efficient:** Receive results in seconds, allowing for quick decision-making.
-
-    ### Get Started
-    Click on the **Disease Recognition** page in the sidebar to upload an image and experience the power of our Plant Disease Recognition System!
-
-    ### About Us
-    Learn more about the project, our team, and our goals on the **About** page.
-    """)
-
-# About Project
-elif (app_mode == "About"):
-    st.header("About")
-    st.markdown("""
-                #### About Dataset
-                This dataset is recreated using offline augmentation from the original dataset.The original dataset can be found on this github repo.
-                This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes.The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
-                A new directory containing 33 test images is created later for prediction purpose.
-                #### Content
-                1. train (70295 images)
-                2. test (33 images)
-                3. validation (17572 images)
-
-                """)
 
 # Prediction Page
+if app_mode == "Disease Recognition":
+    st.header("Disease Recognition")
+
+    test_image = st.file_uploader("Choose an Image:")
+    
+    if test_image is not None and st.button("Show Image"):
+        from PIL import Image
+        image = Image.open(test_image)
+        st.image(image, width=400, use_column_width=True)
 elif (app_mode == "Disease Recognition"):
     st.header("Disease Recognition")
     test_image = st.file_uploader("Choose an Image:")
